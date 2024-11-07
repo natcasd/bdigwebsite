14e1cc752efdaab277496000f615f850754d6430
natcasd <nathan_depiero@brown.edu>
2024-11-06 21:36:49 -0500

    changed description

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports.SITE = {
   name: 'Brown Derivatives Investment Group',
 
-  origin: 'https://tailnext.vercel.app',
+  origin: 'https://bdigwebsite.vercel.app',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'BDIG - Brown Derivatives Investment Group',
+  title: 'Brown Derivatives Investment Group',
   description: 'Official Website of Brown Derivatives Investment Group',
 };
