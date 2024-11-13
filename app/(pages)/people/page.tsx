aba774a5df4117bd1a5371b8b4db4a97dda3061c
natcasd <nathan_depiero@brown.edu>
2024-11-12 20:01:54 -0500

    our team click description place holder till i figure out something better

--- app/(pages)/people/page.tsx
+++ app/(pages)/people/page.tsx
@@ -11,6 +11,7 @@ async function PeoplePage() {
     <section id="peoplePage">
       <div className="mx-auto max-w-7xl py-5 px-4 sm:px-6">
         <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4">Our Team</h1>
+        <p className="text-lg text-gray-600 dark:text-gray-300 italic mb-4">Click to learn more about us</p>
         <hr className="mb-2 border-t-2 border-black dark:border-gray" />
         <div className="py-7 md:py-7">
           <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
